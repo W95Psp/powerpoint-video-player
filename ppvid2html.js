671c20051ea43541e855643ea6ce63bd92ff9635
Lucas Franceschino <lucas.franceschino@inria.fr>
2019-09-27 11:24:52 +0200

    changes

--- ppvid2html.js
+++ ppvid2html.js
@@ -30,8 +30,15 @@ let clientCode = (times, skips) => {
     times = times.filter(x => x!==false);
 
     let g = () => document.getElementById('p');
+    let oldElem = null;
+    let overlayLogic = cur => {
+	let elem = document.querySelector('#overlays > div[data-frame="'+cur+'"]');
+	oldElem && (oldElem.className = '');
+	elem && (elem.className = 'show');
+	oldElem = elem;
+    };
     let gethash = () => +window.location.hash.slice(1);
-    let moveAtEndOf = (specCur = cur) => (g().currentTime = times[specCur][1]);
+    let moveAtEndOf = (specCur = cur) => (overlayLogic(specCur), g().currentTime = times[specCur][1]);
 
     let cur = gethash();
     moveAtEndOf();
@@ -40,20 +47,48 @@ let clientCode = (times, skips) => {
 
     let playAtCur = async () => {
 	let pcur = cur;
+	overlayLogic(pcur);
 	g().currentTime = times[pcur][0];
 	await g().play();
 	setTimeout( () => (g().pause(), moveAtEndOf(pcur))
 		    , 1000 * (times[pcur][1] - times[pcur][0])   );
     };
+    let icur = d => {
+	if(cur + d < times.length && cur + d >= 0)
+	    cur+=d;
+	window.location.hash = cur;
+    }
+    document.body.addEventListener("click", function(event) {
+	icur(+1); playAtCur();
+    });
+    let composeNum = null;
+    let composeNumTO = null;
     document.body.addEventListener("keyup", function(event) {
-	let icur = d => {if(cur + d < times.length && cur + d >= 0) cur+=d; window.location.hash = cur;}
-	if(event.keyCode == 37) { icur(-1); moveAtEndOf(); return false; };
-	if(event.keyCode == 39) { icur(+1); playAtCur();         return false; };
+	if(event.keyCode == 37)
+	  { icur(-1); moveAtEndOf(); return false; };
+	if(event.keyCode == 39)
+	  { icur(+1); playAtCur();   return false; };
+	let n = event.keyCode - 48;
+	if(n >= 0 && n <= 9){
+	    composeNum = (composeNum || 0) * 10 + n;
+	    clearTimeout(composeNumTO);
+	    composeNumTO = setTimeout(() => (composeNum=null), 1000);
+	}
+	if(event.keyCode == 13 && composeNum !== null)
+	    (window.location.hash = cur = composeNum, moveAtEndOf());
     });
 };
 
+let overlays = '';
+try{
+    overlays = require('fs').readFileSync('overlays.html')+'';
+}catch(e){console.error(e);}
+
 console.log(`
-<body><video id="p"><source src="./riotfp0919.mp4" type="video/mp4"></video></body>
+<body>
+<video id="p"><source src="${input}" type="video/mp4"></video>
+<div id='overlays'>${overlays}</div>
+</body>
 <script>(${clientCode.toString()})(${JSON.stringify(out)}, ${JSON.stringify(slides_skip)});</script>
 <style>
   body{ position:absolute;
@@ -65,5 +100,23 @@ console.log(`
     max-height: 100%;
     min-width: 100%;
     min-height: 100%; }
+  #overlays div.show {
+    display: block;
+  }
+  #overlays div {
+    position: absolute;
+    display: none;
+    height:100%;
+    width:100%;
+    left: 0px;
+    top: 0px;
+  }
+  #overlays {
+    position: absolute;
+    height:100%;
+    width:100%;
+    left: 0px;
+    top: 0px;
+  }
 </style>
 `);
